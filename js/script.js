46ea9aa52ce3d9a640532833a3624ca3d4a26f1c
ADILSON-MASILVA <br.adilson.msilva@gmail.com>
2023-10-16 00:08:59 -0300

    Commit_02

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Solicita ao usuário que insira a temperatura em graus Celsius
 var temperaturaCelsius = parseFloat(prompt("Digite a temperatura em graus Celsius:"));
 
-// Converte a temperatura de Celsius para Fahrenheit
+// Converte a temperatura de Celsius para Fahrenheit..
 var temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
 
 // Exibe a temperatura em ambas as escalas no console
